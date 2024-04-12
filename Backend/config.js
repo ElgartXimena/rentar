8af4e33bf1de5dc2325da0428806ffd38ce26e09
Leonel_Leguizamon <leoleguizamon02@gmail.com>
2024-04-12 10:39:51 -0300

    modifs

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb://root:1234@localhost:27017/?retryWrites=true&w=majority';
+export const mongoDBURL = 'mongodb://root:1234@localhost:27017/rentar';
 
